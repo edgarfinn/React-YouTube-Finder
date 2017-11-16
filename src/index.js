f03adbe9ff6eac34f840c9b5200d2c2b6fb72e39
Edgar Dewsbery <edewsbery@gmail.com>
2017-11-16 16:07:10 +0000

    delete console log

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-console.log(process.env.REACT_APP_YT_API_KEY);
 // Create a new component. This component should produce some html.
 
 const App = () => {
