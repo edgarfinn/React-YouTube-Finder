80bf4a7c8da41e5a38baa52cd546b4d8d15d15f7
Edgar Dewsbery <edewsbery@gmail.com>
2017-11-16 16:15:47 +0000

    undo changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+console.log(process.env.REACT_APP_YT_API_KEY);
 // Create a new component. This component should produce some html.
 
 const App = () => {
